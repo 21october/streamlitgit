c9af0f8d73aa4990c5fad251f84386c1cf338557
21october <youkyung.paris@gmail.com>
2023-01-13 11:27:08 +0900

    req6

--- sandro.py
+++ sandro.py
@@ -5,6 +5,7 @@ from urllib.request import urlopen
 import urllib.request
 from bs4 import BeautifulSoup
 import os
+# python3 -m streamlit run /Users/youkyung/21October/streamlitgit/sandro.py
 
 st.title('공식 홈페이지 이미지 다운로드: 산드로')
 
@@ -13,11 +14,11 @@ if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
     st.dataframe(df)
     urls = df['링크']
-    numbers = df['번호']
 
     start_down = st.button(label="다운로드 시작하기")
     if start_down is not None:
-        for url in urls:
+        indx = 1
+        for url in urls:            
             response = requests.get(url)
             html = response.text
             soup = BeautifulSoup(html, 'html.parser')
@@ -31,10 +32,13 @@ if uploaded_file is not None:
                 else:
                     pass
             path = os.getcwd()
+            
             n = 1
             for i in list:
                 with urlopen(i) as f:
-                    with open(path+"/"+name+str(n)+'.jpg','wb') as h:
+                    with open(path+"/"+str(indx)+"_"+name+str(n)+'.jpg','wb') as h:
                         img = f.read()
                         h.write(img)
                 n += 1
+            indx += 1
+    st.success("작업이 완료되었습니다.")
