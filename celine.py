bb6fe13f04493c64d186fab49e06966ea8599f17
21october <youkyung.paris@gmail.com>
2023-01-19 22:03:52 +0900

    k

--- celine.py
+++ celine.py
@@ -12,7 +12,7 @@ st.title('CELINE')
 st.header('공식 홈페이지 이미지 다운로드')
 
 try:
-    os.mkdir("C:\CELINE")
+    os.mkdir("C:\Users\CELINE")
 except:
     pass
 
@@ -41,12 +41,12 @@ if uploaded_file is not None:
                 list.append(imgUrl)
         
             
-            path = "C:\CELINE"
+            path = "C:\Users\CELINE"
             
             n = 1
             for i in list:
                 with urlopen(i) as f:
-                    with open(path+"/"+str(indx)+"_"+name+str(n)+'.jpg','wb') as h:
+                    with open(path+"\"+str(indx)+"_"+name+str(n)+'.jpg','wb') as h:
                         img = f.read()
                         h.write(img)
                 n += 1
